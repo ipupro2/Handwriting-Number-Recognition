12c6a743a86866d418b77033190d0bc16f0f7f85
trucnguyen13 <48061497+trucnguyen13@users.noreply.github.com>
2019-05-14 11:20:33 +0700

    Update KNNClassifier.py

--- Đồ Án 2 CNTT/KNNClassifier.py
+++ Đồ Án 2 CNTT/KNNClassifier.py
@@ -2,31 +2,38 @@ import math
 import random
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
+import numpy as np
 
-def Predict(X_train,y_train,data):
+def Predict(X_train,y_train,data,k):
     x=0
-    for i in range(196):
-        x += (X_train[0][i]-data[i])**2
-    
-    min = math.sqrt(x)
-    
-    x=0
-    k=0
-    for i in range(60000):
+    A = []
+    for i in range(len(X_train)):
+        Arr = []
         for j in range(196):
             x += (X_train[i][j]-data[j])**2
         x = math.sqrt(x)
-        if min > x:
-            min = x
-            k = i
-        x=0
-     
-    return y_train[k] 
+        Arr.append(x)
+        Arr.append(y_train[i])
+        A.append(Arr)
+        x=0  
+        
+    A=np.array(A)
+    A=A[A[:, 0].argsort()]
+    
+    ARR=np.zeros(10)
+    count = 0
+    for i in range(10):
+        for j in range(k):
+            if A[j][1] == i:
+                count+=1
+        ARR[i]=count
+        count = 0
+    return ARR.argmax()
 
 def Test(X_test, y_test, X_train, y_train):
     for i in range(0, 10):
         img = X_test[random.randint(0,len(X_test))]
-        print(Predict(X_train, y_train, img))
+        print(Predict(X_train, y_train, img, 100))
         plt.imshow(img.reshape(14,14), cmap='Greys', interpolation='nearest')
         plt.show()
 #        img = X_test[i]
