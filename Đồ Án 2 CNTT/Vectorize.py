50b1fbcba037ff8f1aceab0ee31ff363fff754c5
trucnguyen13 <48061497+trucnguyen13@users.noreply.github.com>
2019-05-15 11:29:28 +0700

    Update Vectorize.py

--- Đồ Án 2 CNTT/Vectorize.py
+++ Đồ Án 2 CNTT/Vectorize.py
@@ -60,9 +60,9 @@ def median(lst):
     lstLen = len(lst) 
     index = (lstLen - 1) // 2 
     if (lstLen % 2): 
-     return sortedLst[index] 
+        return sortedLst[index] 
     else: 
-     return (sortedLst[index] + sortedLst[index + 1])/2.0 
+        return (sortedLst[index] + sortedLst[index + 1])/2.0 
 
 
 def MinVectorize(X_train, width):
